fcb03c4da8bc2ee115e0cf22e5a9e6d26e4d15eb
Nick <nickoluoch1130@gmail.com>
2024-02-27 18:23:29 +0300

    creating the form section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
-import { useState } from 'react'
+import React,{useState,useEffect,createContext} from 'react'
+import EditSide from './components/editSide'
+import { initialHeaderObject,initialEducationArray,initialExperienceArray } from './intialData'
 import './styles/App.css'
+
+import FormSection from "./components/FormSection";
+import CvSection from "./components/CvSection";
+import ButtonToggleView from "./components/ButtonToggleView";
+
+const FormDataContext = createContext();
 
 function App() {
   
+  const [headerObject, setHeaderObject] = useState(initialHeaderObject);
+  const [educationArray, setEducationArray] = useState(initialEducationArray);
+  const [experienceArray, setExperienceArray] = useState(initialExperienceArray);
 
   return (
-    <div className="app">
-        <h1>Hello</h1>
+    <div className="app-container">
+        <div className="nav-bar">
+          <h1>Make Your CV 📃</h1>
+        </div>
+        <div className="main-content-container">
+           <FormDataContext.Provider
+           value = {{
+            headerObject,
+            setHeaderObject,
+            educationArray,
+            setEducationArray,
+            experienceArray,
+            setExperienceArray
+           }}
+           >
+           <FormSection />
+           <CvSection />
+           </FormDataContext.Provider>
+        </div>
     </div>
   )
 }
---
+++ src/intialData.js
@@ -0,0 +1,61 @@
+import {v4 as uuidv4} from  "uuid"
+
+export const initialHeaderObject = {
+    fullName: "John Doe",
+    email: "johndoe@example.com",
+    phone: "+31 459 789 385",
+    address: "Breda, Netherlands",
+    isOpen: true,
+  }
+
+
+  export const initialEducationArray = [
+    {
+      id: uuidv4(),
+      school: "University of Barcelona",
+      qualification: "Bachelor of Science in Physics",
+      startDate: "Sept 2016",
+      endDate: "June 2021",
+      location: "Barcelona, Spain",
+      isOpen: false,
+    },
+    {
+      id: uuidv4(),
+      school: "Central High School",
+      qualification: "High School Diploma",
+      startDate: "Sept 2012",
+      endDate: "June 2016",
+      location: "Boston, USA",
+      isOpen: false,
+    },
+  ];
+
+
+  export const initialExperienceArray = [
+    {
+      id: uuidv4(),
+      company: "Acme Corporation",
+      position: "Software Engineer",
+      startDate: "Jan 2023",
+      endDate: "Present",
+      location: "Madrid, Spain",
+      description: `- Worked as a Software Engineer in a dynamic team environment, 
+  implementing innovative solutions for various projects. 
+  - Collaborated with cross-functional teams to deliver high-quality software products. 
+  - Participated in code reviews and provided constructive feedback.`,
+      isOpen: false,
+    },
+    {
+      id: uuidv4(),
+      company: "Tech Solutions Inc",
+      position: "Data Scientist",
+      startDate: "Sept 2021",
+      endDate: "Aug 2022",
+      location: "New York City, USA",
+      description: `- Responsible for analyzing large datasets to extract actionable insights 
+  and improve business processes. 
+  - Developed machine learning models to predict customer behavior and optimize marketing strategies. 
+  - Presented findings to stakeholders and provided recommendations for future initiatives.`,
+      isOpen: false,
+    },
+  ];
