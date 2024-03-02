2628a3ec201bc83f3f27f525b40dad40dd504a5b
nickonyi <nickoluoch1130@gmail.com>
2024-03-02 07:54:32 +0300

    education form

--- src/components/EducationForms.jsx
+++ src/components/EducationForms.jsx
@@ -1,6 +1,7 @@
-import { useContext, ChangeEvent, FormEvent } from "react";
+import { useContext, ChangeEvent, FormEvent,useState } from "react";
 import { DragDropContext, Droppable, Draggable} from "react-beautiful-dnd";
 import { FormDataContext } from "../App";
+import { initialEducationArray } from "../intialData";
 import { v4 as uuidv4 } from "uuid";
 
 
@@ -9,13 +10,118 @@ import ExpandLessIcon from "../icons/ExpandLessIcon";
 import ExpandMoreIcon from "../icons/ExpandMoreIcon";
 
 function EducationForm(){
+  const [educationObject,setEducationArray] = useState(initialEducationArray);
+  const [{id, school, qualification, startDate, endDate, location, isOpen }] = educationObject;
+  console.log(educationObject);
+  
+
+  function toggleDropdown(){
+    setEducationArray((prevState)=>
+    prevState.map((edu)=> edu.id === id? {...edu,isOpen:!edu.isOpen}:edu)
+    );
+  }
+
+  const handleChange = (e)=> {
+     const {name,value} = e.target;
+
+     setEducationArray(prevState => 
+      prevState.map(edu => (edu.id === id? {...edu,[name]:value}:edu))
+      )
+  }
+
+  const handleDelete = (e)=> {
+    e.preventDefault();
+    setEducationArray(prevState =>
+      prevState.filter(edu => edu.id !== id)
+      )
+  }
+
+  const handleSave = (e)=> {
+     e.preventDefault();
+     toggleDropdown();
+  }
+
     return (
         <form className="education-form">
             <fieldset>
-                <div className="legend-container" >
+                <div className="legend-container" onClick={toggleDropdown}>
                     <div className="legend-content">
                         <legend>Education</legend>
+                        {!isOpen && qualification && (
+                          <div className="legend-qualification-preview">
+                            <span className="legend-dash">-</span>
+                            {qualification}
+                          </div>
+                        )}
                     </div>
+                    {isOpen?<ExpandLessIcon />:<ExpandMoreIcon/>}
+                </div>
+
+                <div className={`dropdown-menu ${isOpen?'open':''}`}>
+                  <ul>
+                    <li className="input-container">
+                      <label htmlFor={`qualification_${id}`}>Qualification</label>
+                      <input 
+                      type="text" 
+                      id={`qualification_${id}`}
+                      name="qualification"
+                      placeholder="Enterthe qualification name"
+                      value={qualification}
+                      onChange={handleChange}
+                      />
+                    </li>
+                    
+                    <li className="input-container">
+                      <label htmlFor={`school_${id}`}>School</label>
+                      <input
+                        type="text"
+                        id={`school_${id}`}
+                        name="school"
+                        placeholder="Enter the school name"
+                        value={school}
+                        onChange={handleChange}
+                      />
+                    </li>
+                    <li className="input-container">
+                      <label htmlFor={`location_${id}`}>Location</label>
+                      <input
+                        type="text"
+                        id={`location_${id}`}
+                        name="location"
+                        placeholder="Enter the location"
+                        value={location}
+                        onChange={handleChange}
+                      />
+                    </li>
+                    <li className="input-container">
+                      <label htmlFor={`startDate_${id}`}>Start Date</label>
+                      <input
+                        type="text"
+                        id={`startDate_${id}`}
+                        name="startDate"
+                        placeholder="Enter the start date"
+                        value={startDate}
+                        onChange={handleChange}
+                      />
+                    </li>
+                    <li className="input-container">
+                      <label htmlFor={`endDate_${id}`}>End Date</label>
+                      <input
+                        type="text"
+                        id={`endDate_${id}`}
+                        name="endDate"
+                        placeholder="Enter the end date"
+                        value={endDate}
+                        onChange={handleChange}
+                      />
+                    </li>
+                  </ul>
+
+                  <div className="button-container">
+                    <Button className="delete-button" label={"Delete"} onClick={handleDelete} />
+                    <Button className="save-button" label={"Save"} onClick={handleSave} />
+                  </div>
+
                 </div>
             </fieldset>
         </form>
@@ -24,6 +130,7 @@ function EducationForm(){
 
 function EducationForms (){
     const { educationArray, setEducationArray } = useContext(FormDataContext);
+    console.log(educationArray);
 
     const handleAddNew = () => {
         const newEducationEntry = {
@@ -55,16 +162,16 @@ function EducationForms (){
       };
     
     return (
-        <>
-        <div className="forms-title">Education</div>
-        
-       
+      <>
+      <div className="forms-title">Education</div>
+
       <DragDropContext onDragEnd={onDragEnd}>
         <Droppable droppableId="educationFormsContainer">
           {(provided) => (
             <div className="forms-container" {...provided.droppableProps} ref={provided.innerRef}>
               {/* Creates a form synced with each object */}
               {educationArray.map((educationObject, index) => (
+                
                 <Draggable key={educationObject.id} draggableId={educationObject.id} index={index}>
                   {(provided) => (
                     <div
@@ -86,7 +193,7 @@ function EducationForms (){
       </DragDropContext>
 
       <Button className="forms-add-new" label={"+ Add new"} onClick={handleAddNew} />
-        </>
+    </>
     )
 }
 
