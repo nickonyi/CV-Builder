f8f1d8683fccbfae9431c405c198760505859b76
nickonyi <nickoluoch1130@gmail.com>
2024-03-04 11:50:34 +0300

    creating education cv component

--- src/components/CvSection.jsx
+++ src/components/CvSection.jsx
@@ -5,12 +5,16 @@ import html2canvas from "html2canvas";
 import jsPDF from "jspdf"
 
 import HeaderCv from "./HeaderCv";
+import EducationCv from "./EducationCv";
 
 function Cv ({divRef}) {
     
     return (
         <div className="cv" ref={divRef}>
           <HeaderCv />
+          <div className="body-cv">
+            <EducationCv />
+          </div>
         </div>
     )
 }
